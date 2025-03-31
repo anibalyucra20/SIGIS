a9fe8b7f589788720da1814a3b94c6875adfbb39
anibal yucra <anibalyucrac@gmail.com>
2025-03-31 18:16:03 -0500

    Update plantilla.php

--- src/view/plantilla.php
+++ src/view/plantilla.php
@@ -37,11 +37,11 @@ if (isset($_SESSION['sesion_sigi_id']) && isset($_SESSION['sesion_sigi_token']))
     }
     //$arrSesion = $objSesion->verificar_sesion_si_activa($_SESSION['sesion_sigi_id'], $_SESSION['sesion_sigi_token']);
     if (!$response) {
-        echo $response;
+        //echo $response;
         $vista = "login";
     }
 }
-/*
+
 if ($vista == "login" || $vista == "404" || $vista == "intranet") {
     require_once "./src/view/" . $vista . ".php";
 } else {
@@ -50,4 +50,3 @@ if ($vista == "login" || $vista == "404" || $vista == "intranet") {
     include $vista;
     include "./src/view/include/footer.php";
 }
-*/
